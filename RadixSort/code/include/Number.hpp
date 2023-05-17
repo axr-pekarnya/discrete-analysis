975a8a03aaa40369e6cc4bc533c257603bfb3da2
SATANalexander666 <10b1111nika@gmail.com>
2023-05-17 15:41:00 +0300

    Added RadixSort implementation

---
+++ RadixSort/code/include/Number.hpp
@@ -0,0 +1,16 @@
+#ifndef NUMBER_HPP
+#define NUMBER_HPP
+
+#include <string>
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+struct TNumber
+{
+    std::string key;
+    int value;
+};
+
+#endif
+
